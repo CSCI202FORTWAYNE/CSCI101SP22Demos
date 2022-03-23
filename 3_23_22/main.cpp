463fb8e85ad993a50d264c26f24d2e6236d07ea5
Angela Venable <avenable11@ivytech.edu>
2022-03-23 20:25:24 +0000

    final version of 3/23/2022 demo

--- 3_23_22/main.cpp
+++ 3_23_22/main.cpp
@@ -1,38 +1,70 @@
 #include <iostream>
 #include <cmath>
 #include <climits>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
+int getInt(string prompt);
+int secret(int x);
+void resetStream(istream& input);
+
 int main()
 {
-	int memSize = 0;
-	cout << "Enter the memory size in bytes: ";
-	cin >> memSize;
-	cout << endl;
+	
+	int memSize = getInt("Enter the memory size in bytes: ");
 	
 	while(!cin || memSize < 0)
 	{
+		
 		if(!cin)
 		{
-			cin.clear();
-			cin.ignore(INT_MAX, '\n');
+			resetStream(cin);
 		}
 		cout << "Invalid Input!" << endl;
-		cout << "Enter the memory size in bytes: ";
-		cin >> memSize;
-		cout << endl;
+		memSize = getInt("Enter the memory size in bytes: ");
 	}
 	
 	double mar = log2(memSize);
 	
 	cout << "The minimum MAR size is " << ceil(mar) << " bits." << endl;
 	
+	for (int i = 0; i < 10; i++)
+		cout << "Secret 3 is " << secret(3) << endl;
+	
 	return 0;
+
+}
+
+int getInt(string prompt)
+{
+	
+	int usrInt;
+	cout << prompt;
+	cin >> usrInt;
+	cout << endl;
+	
+	return usrInt;
 }
 
 
+void resetStream(istream& input)
+{
+	input.clear();
+	input.ignore(INT_MAX, '\n');
+}
 
+int secret(int x)
+{
+	static int count = 0;
+	cout << "The secret function has been called " << ++count << endl;
+	if(x > 5)
+		return 2*x;
+		
+		
+	return x;
+}
 
 
 
